85b37e9311c87761669ea36311b8825cb75e0656
masaaki1915 <masaaki1915@gmail.com>
2022-08-05 14:58:59 +0900

    add functions for mint

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,16 +1,47 @@
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
+const { ethers } = require("ethers");
 
-const CONTRACT_ADDRESS = "";
-const PUBLIC_KEY = "";
-const PRIVATE_KEY = "";
+const CONTRACT_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const PROVIDER_URL = "http://localhost:8545";
 
 async function mintNFT() {
   const provider = ethers.getDefaultProvider(PROVIDER_URL);
+  const signer = provider.getSigner();
   const contract = require("../artifacts/contracts/MinimalNFT.sol/MinimalNFT.json");
   const nftContract = new ethers.Contract(
     CONTRACT_ADDRESS,
     contract.abi,
-    provider
+    signer
+    // provider
   );
+
+  const nonce = await provider.getTransactionCount(PUBLIC_KEY, "latest");
+  const tx = {
+    nonce,
+    gasPrice: 500000,
+    data: nftContract.methods.mint(PUBLIC_KEY).encodeABI(),
+  };
+
+  const signPromise = signer.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      const tx = signedTx.rawTransaction;
+      if (tx !== undefined) {
+        signer.sendTransaction(tx, function (err, hash) {
+          if (!err) {
+            console.log("The hash of your transaction is: ", hash);
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            );
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err);
+    });
 }
